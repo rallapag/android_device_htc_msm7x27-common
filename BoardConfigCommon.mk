2a50ebb4b6149d9889174183f1d5009466cf9b49
rallapag <shyam.rallapalli@gmail.com>
2013-02-16 14:08:57 +0000

    first commit

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,143 @@
+# Copyright (C) 2011 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+TARGET_SPECIFIC_HEADER_PATH := device/htc/msm7x27-common/include
+
+# inherit from common msm7x27 Recovery
+-include device/htc/msm7x27-common/recovery/BoardConfigCommon.mk
+
+TARGET_ARCH := arm
+TARGET_CPU_ABI := armeabi-v6l
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv6-vfp
+TARGET_BOARD_PLATFORM := msm7x27
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+COMMON_GLOBAL_CFLAGS += -DTARGET_MSM7x27
+
+### Board config
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+
+TARGET_PROVIDES_LIBLIGHT := true
+TARGET_PROVIDES_LIBAUDIO := true
+
+### Kernel
+TARGET_KERNEL_SOURCE := kernel/htc/msm7227
+TARGET_KERNEL_CONFIG := cyanogen_msm7227_defconfig
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
+
+### USB Mass Storage
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/usb_mass_storage/lun0/file
+
+### Wifi related defines
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WLAN_DEVICE                := bcm4329
+BOARD_WPA_SUPPLICANT_DRIVER      := WEXT
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wext
+WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/bcm4329.ko"
+WIFI_DRIVER_MODULE_ARG           := "firmware_path=/vendor/firmware/fw_bcm4329.bin nvram_path=/proc/calibration iface_name=wlan"
+WIFI_DRIVER_MODULE_NAME          := "bcm4329"
+WIFI_DRIVER_FW_PATH_STA          := "/vendor/firmware/fw_bcm4329.bin"
+WIFI_DRIVER_FW_PATH_AP           := "/vendor/firmware/fw_bcm4329_apsta.bin"
+#WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcm4329/parameters/firmware_path"
+BOARD_WLAN_DEVICE_REV            := bcm4329
+WIFI_BAND                        := 802_11_ABG
+
+### Graphics
+# Option for choosing texture target on old adrenos
+BOARD_ADRENO_DECIDE_TEXTURE_TARGET := true
+BOARD_EGL_CFG := device/htc/msm7x27-common/egl.cfg
+TARGET_QCOM_DISPLAY_VARIANT := legacy
+
+COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=60 -DQCOM_HARDWARE -DQCOM_NO_SECURE_PLAYBACK -DQCOM_LEGACY_OMX
+USE_OPENGL_RENDERER := true
+TARGET_USES_C2D_COMPOSITION := false
+TARGET_USES_SF_BYPASS := false
+TARGET_HAVE_BYPASS := false
+TARGET_USES_OVERLAY := false
+TARGET_QCOM_HDMI_OUT := false
+TARGET_GRALLOC_USES_ASHMEM := false
+TARGET_USES_GENLOCK := true
+# Disable HW VSYNC, kernel does not support it
+TARGET_NO_HW_VSYNC := true
+# Some pixel formats aren't supported. Commit used with this flag: http://goo.gl/91KJ9
+COMMON_GLOBAL_CFLAGS += -DQCOM_MISSING_PIXEL_FORMATS
+
+TARGET_DISABLE_TRIPLE_BUFFERING := true
+#TARGET_NO_HW_OVERLAY := true
+#BOARD_EGL_NEEDS_LEGACY_FB := true
+#BOARD_NEEDS_MEMORYHEAPPMEM := true
+#BOARD_USE_ADRENO_200_GPU := true
+
+### Camera
+#BOARD_USES_LEGACY_CAMERA := true
+BOARD_USE_NASTY_PTHREAD_CREATE_HACK := true
+BOARD_NEEDS_MEMORYHEAPPMEM := true
+COMMON_GLOBAL_CFLAGS += -DBINDER_COMPAT
+TARGET_DISABLE_ARM_PIE := true
+
+### Audio
+BOARD_USES_GENERIC_AUDIO := false
+BOARD_PREBUILT_LIBAUDIO := false
+BOARD_USES_QCOM_AUDIO_VOIPMUTE := true
+BOARD_USES_QCOM_AUDIO_RESETALL := true
+
+### Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/htc/msm7x27-common/bluetooth/include
+BOARD_BLUEDROID_VENDOR_CONF := device/htc/msm7x27-common/bluetooth/vnd_msm7x27.txt
+
+### FM radio
+BOARD_HAVE_FM_RADIO := true
+BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
+
+BOARD_VENDOR_QCOM_AMSS_VERSION := 4735
+
+### Boot animation
+TARGET_BOOTANIMATION_USE_RGB565 := true
+TARGET_SCREEN_HEIGHT := 480
+TARGET_SCREEN_WIDTH := 320
+
+### QCOM
+BOARD_USES_QCOM_HARDWARE := true
+BOARD_USES_QCOM_LIBS := true
+BOARD_USES_QCOM_LIBRPC := true
+BOARD_USES_QCOM_GPS := true
+BOARD_USE_QCOM_PMEM := true
+
+###GPS moved here temporarily
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := msm7x27
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 20000
+
+### RIL
+BOARD_USES_LEGACY_RIL := true
+BOARD_USE_NEW_LIBRIL_HTC := true
+
+## Minimal fonts
+SMALLER_FONT_FOOTPRINT := true
+
+### Dalvik
+# If WITH_JIT is configured, build multiple versions of libdvm.so to facilitate
+# correctness/performance bugs triage
+WITH_JIT := true
+
+### Webkit
+TARGET_FORCE_CPU_UPLOAD := true
+
+### Browser
+JS_ENGINE := v8
+HTTP := chrome
+ENABLE_JSC_JIT := true
+ENABLE_WEBGL := true
---
+++ msm7x27.mk
@@ -0,0 +1,156 @@
+#
+# Copyright (C) 2011 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+DEVICE_PACKAGE_OVERLAYS += device/htc/msm7x27-common/overlay
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
+    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.xml \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml \
+    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml
+#    frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+
+# USB
+PRODUCT_COPY_FILES += \
+    device/htc/msm7x27-common/init.msm7x27.usb.rc:root/init.msm7x27.usb.rc
+
+# Media
+PRODUCT_COPY_FILES += \
+    device/htc/msm7x27-common/libaudio/audio_policy.conf:system/etc/audio_policy.conf \
+    device/htc/msm7x27-common/media_codecs.xml:system/etc/media_codecs.xml \
+    device/htc/msm7x27-common/media_profiles.xml:system/etc/media_profiles.xml
+
+# Common msm7x27 firmware
+PRODUCT_COPY_FILES += \
+    device/htc/msm7x27-common/firmware/bcm4329.hcd:system/etc/firmware/bcm4329.hcd \
+    device/htc/msm7x27-common/firmware/fw_bcm4329.bin:system/etc/firmware/fw_bcm4329.bin \
+    device/htc/msm7x27-common/firmware/fw_bcm4329_apsta.bin:system/etc/firmware/fw_bcm4329_apsta.bin
+
+# Bluetooth configuration files
+PRODUCT_COPY_FILES += \
+        system/bluetooth/data/main.le.conf:system/etc/bluetooth/main.conf
+
+# Audio
+PRODUCT_PACKAGES += \
+    audio.a2dp.default \
+    audio_policy.msm7x27 \
+    audio.primary.msm7x27 \
+    libtinyalsa \
+    libaudioutils
+
+### Sensors (Compass, G-Sensor, Proximity, ...)
+PRODUCT_PACKAGES += \
+    sensors.msm7x27 \
+    lights.msm7x27
+
+# Video
+PRODUCT_PACKAGES += \
+    copybit.msm7x27 \
+    gralloc.msm7x27 \
+    libgenlock \
+    libmemalloc \
+    liboverlay \
+    libqdutils \
+    libtilerenderer
+
+# Camera
+PRODUCT_PACKAGES += \
+    camera.msm7x27
+#    libcamera
+
+# QCOM OMX
+PRODUCT_PACKAGES += \
+    libstagefrighthw \
+    libmm-omxcore \
+    libOmxCore
+
+# GPS
+PRODUCT_PACKAGES += \
+    gps.msm7x27 \
+    librpc
+
+# Misc
+PRODUCT_PACKAGES += \
+    com.android.future.usb.accessory \
+    Superuser
+#    brcm_patchram_plus \
+
+### Add system daemons
+PRODUCT_PACKAGES += \
+    rild
+
+# Filesystem management tools
+PRODUCT_PACKAGES += \
+    make_ext4fs \
+    setup_fs
+
+# Graphics
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.opengles.version=131072 \
+    ro.opengles.surface.rgb565=true \
+    com.qc.hardware=true \
+    debug.enabletr=false \
+    debug.hwui.render_dirty_regions=false \
+    debug.qctwa.statusbar=1 \
+    debug.qctwa.preservebuf=1 \
+    hwui.print_config=choice \
+    persist.sys.strictmode.visual=false
+
+# Stagefright
+PRODUCT_PROPERTY_OVERRIDES += \
+    media.stagefright.enable-player=true \
+    media.stagefright.enable-meta=false \
+    media.stagefright.enable-scan=false \
+    media.stagefright.enable-http=true \
+    media.stagefright.enable-aac=true \
+    media.stagefright.enable-qcp=true
+
+# Camera and Camcorder
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.camcorder.disablemeta=1 \
+    ro.htc.camerahack=msm7k
+
+# Media
+PRODUCT_PROPERTY_OVERRIDES += \
+    media.a1026.nsForVoiceRec = 0 \
+    media.a1026.enableA1026 = 1
+
+# For emmc phone storage
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.phone_storage=0
+
+# We have enough storage space to hold precise GC data
+#PRODUCT_TAGS += dalvik.gc.type-precise
+
+### Artwork
+PRODUCT_LOCALES += mdpi
+
+PRODUCT_AAPT_CONFIG := normal mdpi
+PRODUCT_AAPT_PREF_CONFIG := mdpi
+
+# Include Adreno200 vendor blobs
+# http://git.cryptomilk.org/projects/marvel/android_vendor_qcom_msm7x27.git/
+$(call inherit-product, vendor/qcom/msm7x27/qcom-vendor.mk)
